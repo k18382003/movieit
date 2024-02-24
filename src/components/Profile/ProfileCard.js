3995c644066400d73827ab4f15ad870beac8ee22
鄧莉瀕 <k18382003@gmail.com>
2024-02-24 14:45:29 -0800

    Render profile deatil

--- src/components/Profile/ProfileCard.js
+++ src/components/Profile/ProfileCard.js
@@ -7,71 +7,133 @@ import left from '../../assets/icons/swipe-left.png';
 import invitation from '../../assets/icons/send-invitation.png';
 import right from '../../assets/icons/swipe-right.png';
 import './ProfileCard.scss';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import Metrics from './Metrics/Metrics';
+const { REACT_APP_API_BASE_PATH } = process.env;
 
 const ProfileCard = () => {
-  const tempData = {
-    username: 'Summer',
-    genres: 'Horror,Action,Thriller',
-    snacks: 'Popcorn,Chicken nuggets',
-    postalCode: 'V5R 3V5',
-    city: 'Vancouver',
-    days: 'Saturday,Sunday',
-    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in interdum dui. Curabitur vel purus ac metus luctus aliquet sit amet at lacus. Suspendisse pulvinar sapien scelerisque libero pretium blandit. Praesent mollis nisi et diam scelerisque, at luctus risus laoreet. Mauris ultricies dignissim felis. Integer mattis portat. ',
-  };
+  const token = localStorage.getItem('JWTtoken');
+  const [profileDeatil, setProfileDetail] = useState();
+  const [currentUser, setCurrentUser] = useState();
+  const { id } = useParams();
 
   const reStructureString = (input) => {
     let newString = '';
     const arr = input.split(',');
-    arr.forEach((item) => (newString += item + ' / '));
+    arr.forEach(
+      (item) =>
+        (newString +=
+          item[0].toUpperCase() + item.slice(1).toLowerCase() + ' / ')
+    );
     return newString.slice(0, newString.length - 2);
   };
 
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    const fetchProfileDetail = async () => {
+      try {
+        const response = await axios.get(
+          `${REACT_APP_API_BASE_PATH}/profile/${id}`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+
+        console.log('detail', response.data);
+        setProfileDetail({
+          ...response.data,
+          genres: reStructureString(response.data.genres),
+          snacks: reStructureString(response.data.snacks),
+          preferdays: reStructureString(response.data.preferdays),
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfileDetail();
+  }, [id]);
+
+  useEffect(() => {
+    if (!token) {
+      return;
+    }
+
+    const getCurrentUser = async () => {
+      try {
+        const response = await axios.get(
+          `${REACT_APP_API_BASE_PATH}/account/`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(response.data);
+        setCurrentUser(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    getCurrentUser();
+  }, [id]);
+
   return (
-    <section className="profile-card">
-      <div className="profile-card__card-container">
-        <div
-          className="profile-card__image"
-          style={{ backgroundImage: `url(${profilePhoto})` }}
-        ></div>
-        <div className="profile-card__info">
-          <h2 className="profile-card__username">{tempData.username}</h2>
-          <div className="profile-card__data-container">
-            <img className="profile-card__icon" src={like} />
-            <p className="profile-card__data">
-              {reStructureString(tempData.genres)}
-            </p>
+    <>
+      {profileDeatil && (
+        <section className="profile-card">
+          <div className="profile-card__card-container">
+            <div
+              className="profile-card__image"
+              style={{ backgroundImage: `url(${profilePhoto})` }}
+            ></div>
+            <div className="profile-card__info">
+              <h2 className="profile-card__username">
+                {profileDeatil.displayname}
+              </h2>
+              <div className="profile-card__data-container">
+                <img className="profile-card__icon" src={like} />
+                <p className="profile-card__data">{profileDeatil.genres}</p>
+              </div>
+              <div className="profile-card__data-container">
+                <img className="profile-card__icon" src={face} />
+                <p className="profile-card__data">{profileDeatil.snacks}</p>
+              </div>
+              <div className="profile-card__data-container">
+                <img className="profile-card__icon" src={location} />
+                <p className="profile-card__data">
+                  {profileDeatil.postalcode}, {profileDeatil.city}
+                </p>
+              </div>
+              <div className="profile-card__data-container">
+                <img className="profile-card__icon" src={timeLine} />
+                <p className="profile-card__data">{profileDeatil.preferdays}</p>
+              </div>
+              <article className="profile-card__data-container profile-card__data-container--bio">
+                <p className="profile-card__bio ">{profileDeatil.bio}</p>
+              </article>
+            </div>
           </div>
-          <div className="profile-card__data-container">
-            <img className="profile-card__icon" src={face} />
-            <p className="profile-card__data">
-              {reStructureString(tempData.snacks)}
-            </p>
-          </div>
-          <div className="profile-card__data-container">
-            <img className="profile-card__icon" src={location} />
-            <p className="profile-card__data">
-              {tempData.postalCode}, {tempData.city}
-            </p>
-          </div>
-          <div className="profile-card__data-container">
-            <img className="profile-card__icon" src={timeLine} />
-            <p className="profile-card__data">
-              {reStructureString(tempData.days)}
-            </p>
-          </div>
-          <article className="profile-card__data-container profile-card__data-container--bio">
-            <p className="profile-card__bio ">{tempData.bio}</p>
-          </article>
-        </div>
-      </div>
-      <div className="profile-card__buttons">
-        <img src={left} />
-        <img src={invitation} />
-        <img src={right} />
-      </div>
-      {/* <Metrics /> */}
-    </section>
+          {currentUser.userId == id ? (
+            <Metrics />
+          ) : (
+            <div className="profile-card__buttons">
+              <img src={left} />
+              <img src={invitation} />
+              <img src={right} />
+            </div>
+          )}
+        </section>
+      )}
+    </>
   );
 };
 
