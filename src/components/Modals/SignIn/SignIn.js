0d6f4c8c9e4d8ec73a5591d248990dbc2c48b07d
鄧莉瀕 <k18382003@gmail.com>
2024-03-05 09:51:36 -0800

    Deploy testing

--- src/components/Modals/SignIn/SignIn.js
+++ src/components/Modals/SignIn/SignIn.js
@@ -7,7 +7,7 @@ import Button from '../../Button/Button';
 import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router';
-const { REACT_APP_API_BASE_PATH } = process.env;
+const { REACT_APP_API_BASE_PATH } = 'https://movieit-api.fly.dev/movieit/api/';
 
 const SignIn = ({ showModal }) => {
   const navigate = useNavigate();
